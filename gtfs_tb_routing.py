c82c47b2698c1f9ffa9cef3caa92842e1c937c21
Mike Kazantsev <mk.fraggod@gmail.com>
2016-10-10 21:20:10 +0500

    Add grouping of trips into lines

--- gtfs_tb_routing.py
+++ gtfs_tb_routing.py
@@ -26,10 +26,10 @@ get_logger = lambda name: LogStyleAdapter(logging.getLogger(name))
 
 class conf:
 
-	dt_ch = 5*60
+	dt_ch = 5*60 # fixed time-delta overhead for changing trips
 
-	stop_linger_time_default = 5*60
-	footpath_dt_base = dt_ch
+	stop_linger_time_default = 5*60 # used if departure-time is missing
+	footpath_dt_base = dt_ch # footpath_dt = dt_base + km / speed_kmh
 	footpath_speed_kmh = 5 / 3600
 
 
@@ -50,6 +50,7 @@ def parse_gtfs_dts(ts_str):
 	return sum((mul * int(v)) for mul, v in zip([3600, 60, 1], ts_str.split(':')))
 
 def footpath_dt(stop_a, stop_b, math=math):
+	# Calculates footpath time-delta (dt) between two stops
 	# Alternative: use UTM coordinates and KDTree (e.g. scipy) or spatial dbs
 	lon1, lat1, lon2, lat2 = ( math.radians(float(v)) for v in
 		[stop_a.lon, stop_a.lat, stop_b.lon, stop_b.lat] )
@@ -73,7 +74,7 @@ def parse_gtfs_timetable(gtfs_dir):
 	trips, trip_stops = dict(), defaultdict(list)
 	for t in iter_gtfs_tuples(gtfs_dir, 'stop_times'): trip_stops[t.trip_id].append(t)
 	for t in iter_gtfs_tuples(gtfs_dir, 'trips'):
-		trip = trips[t.trip_id] = list()
+		trip = list()
 		for ts in sorted(trip_stops[t.trip_id], key=lambda t: int(t.stop_sequence)):
 			dts_arr, dts_dep = map(parse_gtfs_dts, [ts.arrival_time, ts.departure_time])
 			if not dts_arr:
@@ -82,9 +83,48 @@ def parse_gtfs_timetable(gtfs_dir):
 					else: continue
 				else: dts_arr = trip[-1].dts_dep # "scheduled based on the nearest preceding timed stop"
 			if not dts_dep: dts_dep = dts_arr + conf.stop_linger_time_default
-			trip.append(TripStop(stops[ts.stop_id], dts_arr, dts_dep))
+		if trip: trips[t.trip_id] = trip
 
 	return Timetable(stops, footpaths, trips)
+
+
+def timetable_lines(tt):
+	# Lines - trips with identical stop sequences, ordered from earliest-to-latest by arrival time
+	# If one trip overtakes another (making ordering impossible) - split into diff line
+
+	line_trips = defaultdict(list)
+	line_stops = lambda trip: list(map(op.attrgetter('stop'), trip.stops))
+	for trip in tt.trips: line_trips[line_stops(a)].append(trip)
+
+	lines = list()
+	for trips in line_trips.values():
+		lines_subgroup = list()
+		for a in trips:
+			for trips in lines_subgroup:
+				for b in trips:
+					overtake_check = set( # True: a ≺ b, False: b ≺ a, None: a == b
+						(None if sa.dts_arr == sb.dts_arr else sa.dts_arr <= sb.dts_arr)
+						for sa, sb in zip(a.stops, b.stops) )
+					if True in overtake_check and False in overtake_check: break # diff line
+				else:
+					trips.append(a)
+					break
+			else: lines_subgroup.append([a]) # failed to find line to group trip into
+		for line in lines_subgroup:
+			line.sort(key=lambda trip: sum(map(op.attrgetter('dts_arr'), trip.stops)))
+		lines.extend(lines_subgroup)
+
+	return lines
+
+
+def precalc_transfer_set(tt, lines):
+	raise NotImplementedError
+	for trip in tt.trips:
+		for p in trip[1:]:
+			for q in tt.stops.values():
+				try: dt_fp = tt.footpaths[frozenset([p, q])]
+				except KeyError: continue # unreachable on foot
+				# dt_fp
 
 
 
@@ -100,6 +140,9 @@ def main(args=None):
 	logging.basicConfig(level=logging.DEBUG if opts.debug else logging.WARNING)
 	log = get_logger('main')
 
-	parse_gtfs_timetable(Path(opts.gtfs_dir))
+	tt = parse_gtfs_timetable(Path(opts.gtfs_dir))
+	lines = timetable_lines(tt)
+	transfers = precalc_transfer_set(tt, lines)
+
 
 if __name__ == '__main__': sys.exit(main())
