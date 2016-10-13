95b5e6b5973c1ee428221b1500df59b6ad07990a
Mike Kazantsev <mk.fraggod@gmail.com>
2016-10-13 18:47:18 +0500

    cache: make tree algos a bit more readable

--- tb_routing/cache.py
+++ tb_routing/cache.py
@@ -4,7 +4,10 @@ import re, pickle, base64, hashlib, time
 from . import utils as u
 
 
-class CacheLazySkipped(object): pass
+class CacheCondition(Exception): pass
+class CacheMissing(CacheCondition): pass
+class CacheInvalidated(CacheCondition): pass
+class CacheLazySkipped(CacheCondition): pass
 
 class CalculationCache:
 	'''Wrapper to cache calculation steps to disk.
@@ -54,48 +57,55 @@ class CalculationCache:
 		if lazy:
 			item_re = re.compile('^{}$'.format(
 				re.escape(self.name_fmt.format('@')).replace(r'\@', r'(.*)') ))
-			self.lazy_skip = list(
-				filter(self.cache_valid_check, (
-					item_re.search(p.name).group(1)
-					for p in self.cache_dir.glob(self.name_fmt.format('*')) ) ))
+			self.lazy_skip = list( item_re.search(p.name).group(1)
+					for p in self.cache_dir.glob(self.name_fmt.format('*')) )
 		else: self.lazy_skip = list()
 
 		self.log = u.get_logger('main.cache')
 
 
-	def cache_skip_check(self, func_id, tree=None, chk=None):
-		'Test if cache loading should be skipped as higher-level data is available.'
-		if chk is None:
-			if not self.lazy_skip: return False
-			chk = '\0'.join(self.lazy_skip)
+	def check_skip(self, func_id, tree=None, cached_branch=False):
+		'''Test if cache loading should be skipped as higher-level data is available.
+			Invalidated sub-caches still invalidate whole branch and prevent skipping stuff in it.'''
 		if tree is None: tree = self.dep_tree
-		if not tree: return # reached leaf without finding func_id
-		res_set = set()
+		if not tree: return # reached leaf without finding func_id, no result
+
+		subtree_checks = set()
 		for pat, tree in tree.items():
-			if chk is True:
-				if pat in func_id: return True # func_id in cached branch
-			elif pat in func_id: return False # func_id in uncached branch
-			res = self.cache_skip_check(
-				func_id, tree, chk is True or pat in chk or chk )
-			if res is not None: res_set.add(res)
+			match = pat in func_id
+			if cached_branch:
+				if match: return True # func_id can be skipped
+			elif match: return False
+
+			for func_id_sub in self.lazy_skip:
+				if pat not in func_id_sub: continue
+				if not self.check_valid(func_id_sub): return False # invalidated subtree
+				subtree_cached = True
+				break
+			else: subtree_cached = False
+
+			subtree_checks.add(
+				self.check_skip(func_id, tree, subtree_cached) )
+
 		# Make sure result is skipped only if func_id is
 		#  1) listed in dep-tree and 2) all matches are in cached branches.
-		return True in res_set and False not in res_set
+		return True in subtree_checks and False not in subtree_checks
 
-	def cache_valid_check(self, func_id):
+
+	def check_valid(self, func_id):
 		'Test if cache is invalidated by func_id pattern or dep-change.'
 		if func_id in self.invalidated: return False
 		if any((pat in func_id) for pat in self.invalidate): return False
-		return self._cache_valid_check_tree(func_id, self.dep_tree or dict())
+		return self._check_valid_tree(func_id, self.dep_tree or dict())
 
-	def _cache_valid_check_tree(self, func_id, tree, chk_str=None):
+	def _check_valid_tree(self, func_id, tree, chk_str=None):
 		for pat, tree in tree.items():
-			if pat in func_id:
-				return self._cache_valid_check_tree(
-					func_id, tree, '\0'.join(self.invalidated) )
+			if pat in func_id: # only relevant branch
+				return self._check_valid_tree(
+					func_id, tree, '\0'.join(self.invalidated) ) is not False
 			elif chk_str and pat in chk_str: return False
-			self._cache_valid_check_tree(func_id, tree, chk_str=chk_str)
-		return True
+			subtree_check = self._check_valid_tree(func_id, tree, chk_str=chk_str)
+			if subtree_check is not None: return subtree_check
 
 
 	def serialize(self, data, dst_file): pickle.dump(data, dst_file)
@@ -106,24 +116,25 @@ class CalculationCache:
 
 		if self.cache_dir:
 			cache_file = self.cache_dir / self.name_fmt.format(func_id)
-			if self.cache_skip_check(func_id):
-				self.log.debug('[{}] Lazy-skipped cache-file load: {}', func_id, cache_file.name)
-				return CacheLazySkipped()
-			if cache_file.exists():
-				try:
-					if not self.cache_valid_check(func_id): raise AssertionError
-					with cache_file.open('rb') as src:
-						cache_td = time.monotonic()
-						data = self.unserialize(src)
-						cache_td = time.monotonic() - cache_td
-				except AssertionError as err:
-					self.log.debug('[{}] Invalidated cache: {}', func_id, cache_file.name)
-				except Exception as err:
-					self.log.exception( '[{}] Failed to process cache-file, skipping'
-						' it: {} - [{}] {}', func_id, cache_file.name, err.__class__.__name__, err )
-				else:
-					self.log.debug('[{}] Returning cached result (loaded in {:.1f}s)', func_id, cache_td)
-					return data
+			try:
+				if not self.check_valid(func_id): raise CacheInvalidated
+				if self.check_skip(func_id):
+					self.log.debug('[{}] Lazy-skipped cache-file load: {}', func_id, cache_file.name)
+					return CacheLazySkipped()
+				if not cache_file.exists(): raise CacheMissing
+				with cache_file.open('rb') as src:
+					cache_td = time.monotonic()
+					data = self.unserialize(src)
+					cache_td = time.monotonic() - cache_td
+			except CacheInvalidated:
+				self.log.debug('[{}] Invalidated cache: {}', func_id, cache_file.name)
+			except CacheMissing: pass
+			except Exception as err:
+				self.log.exception( '[{}] Failed to process cache-file, skipping'
+					' it: {} - [{}] {}', func_id, cache_file.name, err.__class__.__name__, err )
+			else:
+				self.log.debug('[{}] Returning cached result (loaded in {:.1f}s)', func_id, cache_td)
+				return data
 		self.invalidated.add(func_id)
 
 		self.log.debug('[{}] Starting...', func_id)
