051b8791cd1d66a25f9497c72b7be33ea3a3a74c
Mike Kazantsev <mk.fraggod@gmail.com>
2016-11-18 00:53:01 +0500

    types.tp: add dump_dot method for exporting any trees to graphviz

--- tb_routing/types/tp.py
+++ tb_routing/types/tp.py
@@ -51,6 +51,33 @@ class TPTree:
 				for node_dict in subtree.values()
 				for node in node_dict.values() ) )
 
+	def dump_dot(self, dst):
+		assert self.prefix, 'Only subtrees are proper graphs'
+		import tb_routing.types as t
+		p = lambda tpl,*a,end='\n',**k: print(tpl.format(*a,**k), file=dst, end=end)
+		def node_name(node):
+			v = node.value
+			if isinstance(v, t.public.Stop): v = v.name
+			elif isinstance(v, t.base.LineStop):
+				v = '{}:{:x}[{}]'.format(node.seed, v.line.id, v.stopidx)
+			else: raise ValueError(type(v), v)
+			return v
+		stops_src, stops_dst = set(), set()
+		p('digraph {{')
+		for k, node_src_set in self.tree.items():
+			for node_seed, node_src in node_src_set.items():
+				name_src = node_name(node_src)
+				if isinstance(node_src.value, t.public.Stop):
+					if node_src.edges_to: stops_src.add(name_src)
+					else: stops_dst.add(name_src)
+				for node_dst in node_src.edges_to:
+					name_dst = node_name(node_dst)
+					p('  "{}" -> "{}"', name_src.replace('"', '\\"'), name_dst.replace('"', '\\"'))
+		for subset in filter(None, [stops_src, stops_dst]):
+			p( 'subgraph {{\n  rank = same;{};\n}}',
+				', '.join('"{}"'.format(n.replace('"', '\\"')) for n in sorted(subset)) )
+		p('}}')
+
 	def path_exists(self, node_src, node_dst):
 		queue = [node_src]
 		while queue:
